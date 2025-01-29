6f740f98a49198af64b8490a9f08f4f11afa11db
PandorTaxe <beaumont.nathan0112@gmail.com>
2025-01-29 11:56:45 +0100

    Random spawn point

---
+++ Assets/Scripts/SpawnManager/SpawnManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpawnManager
+{
+    public class SpawnManager : MonoBehaviour
+    {
+        [SerializeField] List<Transform> _spawnPoints;
+        private List<Transform> _openSpawnPoints;
+
+        void Awake()
+        {
+            _openSpawnPoints = new List<Transform>(_spawnPoints);
+        }
+        
+        public Transform RandomSpawnPoint()
+        {
+            int index = Random.Range(0, _openSpawnPoints.Count);
+            Transform spawnPoint  = _openSpawnPoints[index];
+            _openSpawnPoints.RemoveAt(index);
+            return spawnPoint;
+        }
+    }
+}
