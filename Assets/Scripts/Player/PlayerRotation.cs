983dac290fae2adceb461b604abf5829ffc2eded
PandorTaxe <beaumont.nathan0112@gmail.com>
2025-01-31 12:12:02 +0100

    Fix rotation

--- Assets/Scripts/Player/PlayerRotation.cs
+++ Assets/Scripts/Player/PlayerRotation.cs
@@ -10,7 +10,8 @@ namespace Player
         private Transform _transform;
         private Vector2 _rotation = Vector2.zero;
         private Vector2 _mousePosition = Vector2.zero;
-        private Vector2 _screenMousePosition = Vector2.zero;
+        private Vector3 _worldMousePosition = Vector3.zero;
+        private Vector3 _mouseInput = Vector3.zero;
 
         private void Awake()
         {
@@ -19,20 +20,40 @@ namespace Player
 
         private void Update()
         {
-            _screenMousePosition = cam.ScreenToWorldPoint(_mousePosition);
-            _rotation.Set(_screenMousePosition.x - transform.position.x, _screenMousePosition.y - transform.position.y);
-            _transform.up = _rotation;
+            RotatePlayer();
+        }
+
+        private void RotatePlayer()
+        {
+            _mouseInput.x = Input.mousePosition.x;
+            _mouseInput.y = Input.mousePosition.y;
+            _mouseInput.z = cam.nearClipPlane;
+            Vector3 _worldMousePosition = cam.ScreenToWorldPoint(_mouseInput);
+            _worldMousePosition.z = 0f;
+            RotatePlayerServerRpc(_worldMousePosition);
+        }
+
+        [ServerRpc]
+        private void RotatePlayerServerRpc(Vector3 _worldMousePosition)
+        {
+            if (_worldMousePosition != _transform.position)
+            {
+                 Vector3 targetDirection = _worldMousePosition - _transform.position;
+                targetDirection.z = 0f;
+                _transform.up = targetDirection;
+            }
         }
         
         [ServerRpc]
         private void SetMousePositionServerRpc(Vector2 mousePosition)
         {
             _mousePosition = mousePosition;
-            Debug.Log($"SetMousePositionServerRpc: {mousePosition}");
+            // Debug.Log($"SetMousePositionServerRpc: {mousePosition}");
         }
+        
         public void SetMousePosition(Vector2 mousePosition)
         {
-            SetMousePositionServerRpc(mousePosition);
+            SetMousePositionServerRpc(Input.mousePosition);
         }
     }
 }
