21fe8081d7f19aa2ce3a9ee9aabeea095b1034ac
xinom4966 <sd.dessay@gmail.com>
2025-02-04 17:30:54 +0100

    Started the main menu
    Still needs to connect to a lobby and display the tutorial and credits

---
+++ Assets/Scripts/MenuScripts/MenuScript.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+    public void StartGame()
+    {
+        //Insérer code pour rechercher et rejoindre un lobby
+        Debug.Log("Jouer");
+    }
+}
