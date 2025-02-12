b3454cc7918f962c9124081bcf3ce1d7463185f4
PandorTaxe <beaumont.nathan0112@gmail.com>
2025-02-12 11:05:40 +0100

    Add Timer to Player UI

--- Assets/Scripts/Lobby/TimeManager.cs
+++ Assets/Scripts/Lobby/TimeManager.cs
@@ -6,9 +6,11 @@ namespace Lobby
 {
     public class TimeManager : NetworkBehaviour
     {
-        [SerializeField] private float _gameCloseLobbyTime = 600;
-        [SerializeField] private float _gameTimer;
+        [SerializeField] private float _gameCloseLobbyTime = 600f;
         private readonly UnityEvent _onTimerFinished = new();
+        private readonly UnityEvent<int,int> _onUpdateUI = new();
+        private int _minutes;
+        private int _seconds;
         
         public override void OnNetworkSpawn()
         {
@@ -22,14 +24,15 @@ namespace Lobby
         {
             if(IsServer)
             {
-                _gameTimer += Time.deltaTime;
-                if (_gameTimer >= _gameCloseLobbyTime)
-                {
-                    _onTimerFinished.Invoke();
-                }
+                if (_gameCloseLobbyTime >= 0)
+                    _gameCloseLobbyTime -= Time.deltaTime;
+                _minutes = Mathf.FloorToInt(_gameCloseLobbyTime / 60f);
+                _seconds = Mathf.FloorToInt(_gameCloseLobbyTime - _minutes * 60f);
+                _onUpdateUI.Invoke(_minutes, _seconds);
             }
         }
         
         public UnityEvent OnTimerFinished => _onTimerFinished;
+        public UnityEvent<int, int> OnUpdateUI => _onUpdateUI;
     }
 }
